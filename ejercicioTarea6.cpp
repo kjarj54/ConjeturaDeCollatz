60da55a1601cbf65d965e9aa68ec7d6d8cb9ad4b
Arauz <86898345+kjarj54@users.noreply.github.com>
2023-04-06 08:57:50 -0600

    a

--- ejercicioTarea6.cpp
+++ ejercicioTarea6.cpp
@@ -1,8 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
 #include <iostream>
 
 using namespace std;
 
-int main(){
+void collatz(int num) {
+  while (num != 1) {
+    printf("%d, ", num);
+    if (num % 2 == 0)
+      num = num / 2;
+    else
+      num = 3 * num + 1;
+  }
+  printf("1\n");
+}
 
-    return 0;
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    cout<<("Usage: %s <number>\n", argv[0]);
+    return 1;
+  }
+
+  int num = atoi(argv[1]);
+
+  if (num <= 0) {
+    cout<<("Error: Entrada no valida\n");
+    return 1;
+  }
+
+  pid_t pid = fork();
+
+  if (pid == -1) {
+    cout<<("Fork failed\n");
+    return 1;
+  }
+
+  if (pid == 0) { 
+    collatz(num);
+    exit(0);
+  } else {
+    wait(NULL);
+   	cout<<("Proceso hijo terminado\n");
+  }
+
+  return 0;
 }
